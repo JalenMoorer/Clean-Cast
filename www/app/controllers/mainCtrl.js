31311468ed9d3430aae54a0dc12775c8c9fedbba
Jalen <jalenmmoorer@gmail.com>
2015-10-18 22:36:16 -0400

    Added hourly and daily weather info

--- www/app/controllers/mainCtrl.js
+++ www/app/controllers/mainCtrl.js
@@ -11,11 +11,46 @@ angular.module('mainCtrl', ['ionic', 'ngCordova'])
 
     self.getWeather = function(lat, long) {
         Weather.getWeather(lat, long).success(function(data){
-            self.currently = data.currently;
-            self.hourly = data.hourly;
-            self.daily = data.daily;
-            console.log(data);
+            var currently = data.currently;
+            var hourly = data.hourly.data;
+            var daily = data.daily.data;
+            var alert = data.alerts[0];
+
+            self.currently = currently;
+            self.hourly = self.convertHour(hourly);
+            self.daily = self.convertTime(daily);
+            self.alert = alert;
         });
+    };
+
+    self.convertTime = function(daily) {
+        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        var months = ["Jan", "Feb", "March", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+        for (var i=0; i < daily.length; i++) {
+            var time = new Date(daily[i].time * 1000);
+            var day = time.getDay();
+            var month = time.getMonth() + 1;
+            var date = time.getDate();
+
+            daily[i].time = days[day] + " " + months[month] + " " + date; 
+        }
+
+        return daily;
+    };
+
+    self.convertHour = function(hourly) {
+        for (var i=0; i < hourly.length; i++) {
+            var time = new Date(hourly[i].time * 1000);
+            var hour = time.getHours();
+            var time_period = hour < 12 ? 'AM' : 'PM';
+
+            if (hour > 12) hour = hour % 12;
+            if (self.isMidnight(hour)) hour = "12";
+
+            hourly[i].time = hour + " " + time_period;
+        }
+
+        return hourly;
     };
 
     self.getTime = function() {
