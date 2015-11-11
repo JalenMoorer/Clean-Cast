0a2fb89a25fdeccc1055a410376b2cf3b65a8def
Jalen <jalenmmoorer@gmail.com>
2015-11-11 14:29:26 -0500

    removed forecast io key

--- www/app/services/weather.js
+++ www/app/services/weather.js
@@ -1,14 +1,11 @@
 angular.module('weatherService', ['ngCordova'])
 
-.constant('FORECASTIO_KEY', '7eaa3d726ceecc1937bb96a6be95b13c')
-
 .factory('Weather', function ($http, $q, FORECASTIO_KEY) {
 
 	var url = 'http://jalenscript.com/api/fetch/';
 
     return {
         getWeather: function(lat, long)  {
-            //return $http.jsonp(url + lat + ',' + long + "?callback=JSON_CALLBACK");
             return $http.get(url + '?lat=' + lat + '&lng=' + long);
         }
     };
